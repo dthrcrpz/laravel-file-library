c04b6365037cffd5d01fefada4275db94a5bfda3
teryong <dthrcrpz@gmail.com>
2021-11-23 21:15:01 +0800

    config is now loaded first on the provider

--- src/Providers/FileServiceProvider.php
+++ src/Providers/FileServiceProvider.php
@@ -8,11 +8,11 @@ class FileServiceProvider extends ServiceProvider
 {
     public function boot ()
     {
+        $this->mergeConfigFrom(__DIR__.'/../config/filelibrary.php', 'filelibrary');
+
         $this->loadRoutesFrom(__DIR__.'/../routes/files.php');
 
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations/2021_11_23_00000_create_files_table.php');
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations/2021_11_23_00001_create_file_attachments_table.php');
-        
-        $this->mergeConfigFrom(__DIR__.'/../config/filelibrary.php', 'filelibrary');
     }
 }
