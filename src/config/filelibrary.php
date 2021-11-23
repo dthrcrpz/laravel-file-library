75945940e781a7ee46c2a001cda95d8a462b4846
teryong <dthrcrpz@gmail.com>
2021-11-23 18:00:16 +0800

    added s3_url to config

--- src/config/filelibrary.php
+++ src/config/filelibrary.php
@@ -8,4 +8,12 @@ return [
     | Supported: "public", "s3"
     */
     'storage' => 'public',
+    
+    /*
+    |--------------------------------------------------------------------------
+    | AWS S3 URL
+    |--------------------------------------------------------------------------
+    | Example Format: https://your-domain.s3-ap-southeast-1.amazonaws.com/
+    */
+    's3_url' => env('S3_URL', null)
 ];
